6b02951b6900b26daae83c1812150891056b3064
zeindevs <zeindevs@gmail.com>
2024-04-20 14:55:01 +0700

    feat: add forget password

---
+++ src/pages/auth/forget-password.tsx
@@ -0,0 +1,80 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import Layout from '@/components/layout'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { toast } from '@/components/ui/use-toast'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: 'Invalid email',
+  }),
+})
+
+export default function ForgetPassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  useEffect(() => {
+    window.document.title = 'Forget Password'
+  }, [])
+
+  return (
+    <Layout>
+      <div className="py-16">
+        <Card className="mx-auto max-w-[400px]">
+          <CardHeader>
+            <CardTitle>Forget password</CardTitle>
+            <CardDescription>Reset password will be send to your email account.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="example@gmail.com" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="w-full">
+                  Submit
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </Layout>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/auth/login'
 import SignUp from './pages/auth/signup'
+import ForgetPassword from './pages/auth/forget-password'
 
 export const router = createBrowserRouter([
   {
@@ -16,5 +17,9 @@ export const router = createBrowserRouter([
   {
     path: '/register',
     element: <SignUp />,
+  },
+  {
+    path: '/forget-password',
+    element: <ForgetPassword />,
   },
 ])
